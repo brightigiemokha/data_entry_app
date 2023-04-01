5d2dc71a87b31bc28dad782721257cadae657da2
brightigiemokha <brightigiemokha@yahoo.com>
2023-04-01 13:41:22 +0000

    start game

--- run.py
+++ run.py
@@ -1,110 +1,162 @@
-import tkinter
-from tkinter import ttk
-from tkinter import messagebox
+import random
+import gspread
 
-def enter_data():
-    accepted = accept_var.get()
-    
-    if accepted=="Accepted":
-        # User info
-        firstname = first_name_entry.get()
-        lastname = last_name_entry.get()
-        
-        if firstname and lastname:
-            title = title_combobox.get()
-            age = age_spinbox.get()
-            nationality = nationality_combobox.get()
-            
-            # Course info
-            registration_status = reg_status_var.get()
-            numcourses = numcourses_spinbox.get()
-            numsemesters = numsemesters_spinbox.get()
-            
-            print("First name: ", firstname, "Last name: ", lastname)
-            print("Title: ", title, "Age: ", age, "Nationality: ", nationality)
-            print("# Courses: ", numcourses, "# Semesters: ", numsemesters)
-            print("Registration status", registration_status)
-            print("------------------------------------------")
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman')
+# read the list of words from google sheets
+
+word_list = []
+words = SHEET.worksheet("words")
+word_list = words.col_values(1)
+
+def get_word():
+    word = random.choice(word_list)
+    return word.upper()
+
+
+def play(word):
+    word_completion = "_" * len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play Hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in guessed_letters:
+                print("You already guessed the letter", guess)
+            elif guess not in word:
+                print(guess, "is not in the word.")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("Good job,", guess, "is in the word!")
+                guessed_letters.append(guess)
+                word_as_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion = "".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word.")
+                tries -= 1
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
         else:
-            tkinter.messagebox.showwarning(title="Error", message="First name and last name are required.")
+            print("Not a valid guess.")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
     else:
-        tkinter.messagebox.showwarning(title= "Error", message="You have not accepted the terms")
+        print("Sorry, you ran out of tries. The word was " + word + ". Maybe next time!")
 
-window = tkinter.Tk()
-window.title("Data Entry Form")
 
-frame = tkinter.Frame(window)
-frame.pack()
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
 
-# Saving User Info
-user_info_frame =tkinter.LabelFrame(frame, text="User Information")
-user_info_frame.grid(row= 0, column=0, padx=20, pady=10)
 
-first_name_label = tkinter.Label(user_info_frame, text="First Name")
-first_name_label.grid(row=0, column=0)
-last_name_label = tkinter.Label(user_info_frame, text="Last Name")
-last_name_label.grid(row=0, column=1)
+def main():
+    word = get_word()
+    play(word)
+    while input("Play Again? (Y/N) ").upper() == "Y":
+        word = get_word()
+        play(word)
 
-first_name_entry = tkinter.Entry(user_info_frame)
-last_name_entry = tkinter.Entry(user_info_frame)
-first_name_entry.grid(row=1, column=0)
-last_name_entry.grid(row=1, column=1)
 
-title_label = tkinter.Label(user_info_frame, text="Title")
-title_combobox = ttk.Combobox(user_info_frame, values=["", "Mr.", "Ms.", "Dr."])
-title_label.grid(row=0, column=2)
-title_combobox.grid(row=1, column=2)
-
-age_label = tkinter.Label(user_info_frame, text="Age")
-age_spinbox = tkinter.Spinbox(user_info_frame, from_=18, to=110)
-age_label.grid(row=2, column=0)
-age_spinbox.grid(row=3, column=0)
-
-nationality_label = tkinter.Label(user_info_frame, text="Nationality")
-nationality_combobox = ttk.Combobox(user_info_frame, values=["Africa", "Antarctica", "Asia", "Europe", "North America", "Oceania", "South America"])
-nationality_label.grid(row=2, column=1)
-nationality_combobox.grid(row=3, column=1)
-
-for widget in user_info_frame.winfo_children():
-    widget.grid_configure(padx=10, pady=5)
-
-# Saving Course Info
-courses_frame = tkinter.LabelFrame(frame)
-courses_frame.grid(row=1, column=0, sticky="news", padx=20, pady=10)
-
-registered_label = tkinter.Label(courses_frame, text="Registration Status")
-
-reg_status_var = tkinter.StringVar(value="Not Registered")
-registered_check = tkinter.Checkbutton(courses_frame, text="Currently Registered",
-                                       variable=reg_status_var, onvalue="Registered", offvalue="Not registered")
-
-registered_label.grid(row=0, column=0)
-registered_check.grid(row=1, column=0)
-
-numcourses_label = tkinter.Label(courses_frame, text= "# Completed Courses")
-numcourses_spinbox = tkinter.Spinbox(courses_frame, from_=0, to='infinity')
-numcourses_label.grid(row=0, column=1)
-numcourses_spinbox.grid(row=1, column=1)
-
-numsemesters_label = tkinter.Label(courses_frame, text="# Semesters")
-numsemesters_spinbox = tkinter.Spinbox(courses_frame, from_=0, to="infinity")
-numsemesters_label.grid(row=0, column=2)
-numsemesters_spinbox.grid(row=1, column=2)
-
-for widget in courses_frame.winfo_children():
-    widget.grid_configure(padx=10, pady=5)
-
-# Accept terms
-terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions")
-terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=10)
-
-accept_var = tkinter.StringVar(value="Not Accepted")
-terms_check = tkinter.Checkbutton(terms_frame, text= "I accept the terms and conditions.",
-                                  variable=accept_var, onvalue="Accepted", offvalue="Not Accepted")
-terms_check.grid(row=0, column=0)
-
-# Button
-button = tkinter.Button(frame, text="Enter data", command= enter_data)
-button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
- 
-window.mainloop()
+if __name__ == "__main__":
+    main()
